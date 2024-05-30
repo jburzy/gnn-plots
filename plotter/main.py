02e1a65ae0b221631141068086a9db43f9fdb4dc
jburzy <jackson.carl.burzynski@cern.ch>
2024-05-30 12:05:08 -0700

    black formatting

--- plotter/main.py
+++ plotter/main.py
@@ -5,10 +5,10 @@ import importlib
 
 
 def gnn_plots(config_file):
-
     def tuple_constructor(loader, node):
         return tuple(loader.construct_sequence(node))
-    yaml.SafeLoader.add_constructor('tag:yaml.org,2002:python/tuple', tuple_constructor)
+
+    yaml.SafeLoader.add_constructor("tag:yaml.org,2002:python/tuple", tuple_constructor)
 
     run_config = {}
     with open(config_file) as f:
@@ -18,17 +18,17 @@ def gnn_plots(config_file):
             print("A YAML exception occurred:\n", err)
             sys.exit(1)
 
-    for _, plot_config in run_config['plots'].items():
-        if 'class' not in plot_config:
+    for _, plot_config in run_config["plots"].items():
+        if "class" not in plot_config:
             raise ValueError("YAML configuration must contain a 'class' property")
-        class_name = plot_config.pop('class')
-        module_name, class_name = class_name.rsplit('.', 1)
+        class_name = plot_config.pop("class")
+        module_name, class_name = class_name.rsplit(".", 1)
         try:
             module = importlib.import_module(module_name)
             cls = getattr(module, class_name)
         except (ImportError, AttributeError) as e:
             raise ImportError(f"Cannot find class {class_name} in module {module_name}") from e
-        
+
         plot_obj = cls(**plot_config)
         plot_obj.plot()
 
@@ -39,6 +39,7 @@ def main():
     args = parser.parse_args()
 
     gnn_plots(args.config)
+
 
 if __name__ == "__main__":
     main()
--- plotter/plot_classes/roc_plot.py
+++ plotter/plot_classes/roc_plot.py
@@ -5,28 +5,37 @@ import h5py
 import numpy as np
 import pandas as pd
 from plotter.plot_classes.plotbase import PlotBase
-    
+
+
 class RocPlotBase(PlotBase):
     def plot(self):
         required_params = {
-            'n_ratio_panels', 'ylabel', 'xlabel', 'atlas_second_tag', 'figsize', 'y_scale'
+            "n_ratio_panels",
+            "ylabel",
+            "xlabel",
+            "atlas_second_tag",
+            "figsize",
+            "y_scale",
         }
-        filtered_params = {key: value for key, value in self.config.style.items() if key in required_params}
+        filtered_params = {
+            key: value for key, value in self.config.style.items() if key in required_params
+        }
         roc_plot = RocPlot(**filtered_params)
 
         for _, sample in self.config.samples.items():
             sample_config = ConfigDict(sample)
-            with h5py.File(sample_config.path, 'r') as hdf_file:
-
+            with h5py.File(sample_config.path, "r") as hdf_file:
                 ds = hdf_file[sample_config.df_name]
 
                 target_label = self.config.target_label
                 tagger_output = self.config.tagger_output
 
-                df = pd.DataFrame({
-                    target_label: np.array(ds[target_label]).transpose(), 
-                    tagger_output: np.array(ds[tagger_output]).transpose(),
-                })
+                df = pd.DataFrame(
+                    {
+                        target_label: np.array(ds[target_label]).transpose(),
+                        tagger_output: np.array(ds[tagger_output]).transpose(),
+                    }
+                )
                 df = df.dropna()
 
                 # defining boolean arrays to select the different flavour classes
@@ -38,7 +47,9 @@ class RocPlotBase(PlotBase):
 
                 n_pu = sum(is_pu)
 
-                rej = calc_rej(df[is_hs]["GN2ej_pdispjet"].values, df[is_pu]["GN2ej_pdispjet"].values, sig_eff)
+                rej = calc_rej(
+                    df[is_hs]["GN2ej_pdispjet"].values, df[is_pu]["GN2ej_pdispjet"].values, sig_eff
+                )
 
                 # here the plotting of the roc starts
                 roc_plot.add_roc(
